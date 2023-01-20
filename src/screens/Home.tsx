50665a088736ec7d8b1ed3e7e5c30800496a1def
GuilhermeBiscalchin <desenvolvendogame@gmail.com>
2023-01-20 17:58:24 -0300

    Terminando a Rota

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import {useNavigation} from '@react-navigation/native'
 
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning.ts'
 
@@ -10,6 +11,8 @@ const minimusSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimusSummaryDatesSizes - dateFromYearStart.length
 
 export function Home() {
+
+    const {navigate} = useNavigation()
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -33,7 +36,9 @@ export function Home() {
                 <View className="flex-row flex-wrap">
                     {
                         dateFromYearStart.map(date => {
-                            return <HabitDay key={date.toString()} />
+                            return <HabitDay key={date.toISOString()}
+                            onPress = {() => navigate('habit', {date: date.toISOString()})}
+                            />
                         })
                     }
 
